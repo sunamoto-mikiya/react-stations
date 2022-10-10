36cc357b70155cd1c6b507a54ec300fee991e568
SunamotoMikiya <miki38gtr@gmail.com>
2022-10-10 17:39:56 +0900

    分と画像を追加

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
     <div>
-      <header>Hello World</header>
+      <div>
+        <header>Hello World</header>
+      </div>
+      <div>
+        <a>これは犬の画像を出すページです</a>
+      </div>
+      <div>
+        <img src="https://images.dog.ceo/breeds/hound-plott/hhh-23456.jpeg" />
+      </div>
     </div>
   )
 }
